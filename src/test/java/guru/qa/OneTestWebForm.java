67fd09be4f4756eb767183b545259db5a53e136a
elizavetaalimbekova <Alimbekova.elizaveta98@mail.ru>
2021-07-11 20:03:31 +0300

    Added fixes

--- src/test/java/guru/qa/OneTestWebForm.java
+++ src/test/java/guru/qa/OneTestWebForm.java
@@ -13,7 +13,7 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class oneTestWebForm {
+public class OneTestWebForm {
     @BeforeAll
     static void setup() {
         Configuration.baseUrl = "https://demoqa.com";
@@ -27,19 +27,19 @@ public class oneTestWebForm {
         $("#firstName").setValue("Liza");
         $("#lastName").setValue("Shilo");
         $("#userEmail").setValue("alim@mail.ru");
-                $(byText("Female")).click();
-                $("#userNumber").setValue("9626974543");
-                $("#dateOfBirthInput").clear();
-                $(".react-datepicker__month-select").selectOption("October");
-                $(".react-datepicker__year-select").selectOption("1998");
-                $(".react-datepicker__day--025").click();
-                $("#subjectsInput").setValue("Computer Science").pressEnter();
-                $("#hobbiesWrapper").$(byText("Reading")).click();
-                $("#uploadPicture").uploadFile(new File("src/test/resources/Picture.png"));
-                $("#currentAddress").setValue("my address");
-                $("#react-select-3-input").setValue("Rajasthan").pressEnter();
-                $("#react-select-4-input").setValue("Jaipur").pressEnter();
-                $("#submit").click();
+        $(byText("Female")).click();
+        $("#userNumber").setValue("9626974543");
+        $("#dateOfBirthInput").clear();
+        $(".react-datepicker__month-select").selectOption("October");
+        $(".react-datepicker__year-select").selectOption("1998");
+        $(".react-datepicker__day--025").click();
+        $("#subjectsInput").setValue("Computer Science").pressEnter();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/Picture.png"));
+        $("#currentAddress").setValue("my address");
+        $("#react-select-3-input").setValue("Rajasthan").pressEnter();
+        $("#react-select-4-input").setValue("Jaipur").pressEnter();
+        $("#submit").click();
 
 
                 //check date
@@ -56,8 +56,6 @@ public class oneTestWebForm {
         $(".modal-body").shouldHave(text("my address"));
         $(".modal-body").shouldHave(text("Rajasthan"));
         $(".modal-body").shouldHave(text("Jaipur"));
-
-        $(".modal-body").shouldHave(text("Liza"), text("Shilo"), text("Female"));
 
 
     }
